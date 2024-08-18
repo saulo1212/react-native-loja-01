903b412c3c35035e3bc0b62e6759ca8a923b2c9d
saulo1212 <saulodetacio715@gmail.com>
2024-08-18 15:23:07 -0300

    Types

---
+++ my-app/types/category.ts
@@ -0,0 +1,5 @@
+export type Category = {
+    id: number,
+    title: String,
+    cover: String
+}
---
+++ my-app/types/product.ts
@@ -0,0 +1,8 @@
+export type Product = {
+    id: number,
+    idCategory: number,
+    image: String,
+    title: String,
+    description: String,
+    price: number
+}
